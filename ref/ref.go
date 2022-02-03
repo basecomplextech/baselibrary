eda9ab15f2efbc7c7b5def4ffc7f6b9dce18deba
Ivan Korobkov <ivan.korobkov@gmail.com>
2022-02-03 15:59:05 +0300

    ref: Refcounts in methods.

--- ref/ref.go
+++ ref/ref.go
@@ -8,10 +8,10 @@ type Ref interface {
 	Acquire() bool
 
 	// Retain increments the count, panics when the count was 0.
-	Retain()
+	Retain() int32
 
 	// Release decrements the count and releases the object when the count is 0.
-	Release()
+	Release() int32
 
 	// Refcount returns the current reference count.
 	Refcount() int32
@@ -46,18 +46,19 @@ func (r *refImpl) Acquire() bool {
 	return v > 1
 }
 
-func (r *refImpl) Retain() {
+func (r *refImpl) Retain() int32 {
 	v := atomic.AddInt32(&r.count, 1)
 	if v <= 1 {
 		panic("cannot retain an already released reference")
 	}
+	return v
 }
 
-func (r *refImpl) Release() {
+func (r *refImpl) Release() int32 {
 	v := atomic.AddInt32(&r.count, -1)
 	switch {
 	case v > 0:
-		return
+		return v
 	case v < 0:
 		panic("cannot release an already released reference")
 	}
@@ -65,6 +66,7 @@ func (r *refImpl) Release() {
 	if r.r != nil {
 		r.r.Released()
 	}
+	return v
 }
 
 func (r *refImpl) Refcount() int32 {
