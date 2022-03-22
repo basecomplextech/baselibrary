29bdf99c2ca7d008daa3a4c9461a64b5829f895e
Ivan Korobkov <ivan.korobkov@gmail.com>
2022-03-22 17:04:09 +0300

    alloc: Merged block and size files

--- alloc/block.go
+++ alloc/block.go
@@ -1,5 +1,12 @@
 package alloc
 
+const (
+	minBlockSize = 1 << 10 // 1024
+	maxBlockSize = 1 << 24 // 16MB
+)
+
+var blockClassSizes []int // powers of two
+
 type block struct {
 	buf []byte
 }
@@ -33,4 +40,21 @@ func (b *block) reset() {
 	}
 
 	b.buf = b.buf[:0]
+}
+
+// getBlockClass returns an index in blockClassSizes, or -1.
+func getBlockClass(size int) int {
+	for cls, blockSize := range blockClassSizes {
+		if size > blockSize {
+			continue
+		}
+		return cls
+	}
+	return -1
+}
+
+func initBlockClasses() {
+	for size := minBlockSize; size <= maxBlockSize; size *= 2 {
+		blockClassSizes = append(blockClassSizes, size)
+	}
 }
