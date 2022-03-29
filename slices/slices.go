f24313beed2d4d1ecb3887d76d9fe3a8fefccaf4
Ivan Korobkov <ivan.korobkov@gmail.com>
2022-03-29 08:31:44 +0300

    slices: Generic slice functions

---
+++ slices/slices.go
@@ -0,0 +1,30 @@
+package slices
+
+import (
+	"math/rand"
+	"sort"
+
+	"github.com/baseblck/library/constraints"
+)
+
+// Reverse reverse the slice in place.
+func Reverse[T any](s []T) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
+
+// Shuffle pseudo-randomizes the order of elements using rand.Shuffle.
+func Shuffle[T any](s []T) {
+	rand.Shuffle(len(s), func(i, j int) {
+		s[i], s[j] = s[j], s[i]
+	})
+}
+
+// Sort sorts a slice of ordered items.
+func Sort[T constraints.Ordered](s []T) {
+	sort.Slice(s, func(i, j int) bool {
+		a, b := s[i], s[j]
+		return a < b
+	})
+}
