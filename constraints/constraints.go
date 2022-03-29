3dcfa83d8c7529591d0633bc6a0fe1ff52001320
Ivan Korobkov <ivan.korobkov@gmail.com>
2022-03-29 08:31:28 +0300

    constraints: Contraints for generics

---
+++ constraints/constraints.go
@@ -0,0 +1,31 @@
+package constraints
+
+// Integer specifies all integers.
+type Integer interface {
+	Signed | Unsigned
+}
+
+// Signed specifies signed integers.
+type Signed interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64
+}
+
+// Unsigned specifies unsigned integers.
+type Unsigned interface {
+	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr
+}
+
+// Float specifies all float-point types.
+type Float interface {
+	~float32 | ~float64
+}
+
+// Complex specifies all complex types.
+type Complex interface {
+	~complex64 | ~complex128
+}
+
+// Ordered specifies types which support comparision.
+type Ordered interface {
+	Integer | Float | ~string
+}
