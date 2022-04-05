16c33c864b3fbc2372e72795d49431ce8a6e20db
Ivan Korobkov <ivan.korobkov@gmail.com>
2022-04-05 15:51:58 +0300

    encoding/compactint: Compact integer, #6

---
+++ encoding/compactint/int.go
@@ -0,0 +1,98 @@
+// package compactint provides a compact variable length integer encoding.
+//
+// Everything is big-endian.
+//
+// Encoding
+//
+//  Size                   Prefix  Example               Description
+//	<= 0xfc                -       0x12                  uint8_t
+//	<= 0xffff              0xfd    0xfd1234              0xfd followed by uint16
+//	<= 0xffffffff          0xfe    0xfe12345678          0xfe followed by uint32
+//	<= 0xffffffffffffffff  0xff    0xff1234567890abcdef  0xff followed by uint64
+//
+package compactint
+
+import (
+	"encoding/binary"
+	"math"
+)
+
+const MaxLen = 9
+
+// Uint32 returns a uint32 value and the number of bytes read.
+func Uint32(b []byte) (uint32, int) {
+	v, n := Uint64(b)
+	if n <= 0 {
+		return 0, n
+	}
+	if v > math.MaxUint32 {
+		return 0, -1
+	}
+	return uint32(v), n
+}
+
+// Uint64 returns a uint64 value and the number of bytes read.
+func Uint64(b []byte) (uint64, int) {
+	if len(b) == 0 {
+		return 0, 0
+	}
+
+	f := b[0]
+	switch f {
+	default:
+		return uint64(f), 1
+
+	case 0xfd:
+		if len(b) < 3 {
+			return 0, 0
+		}
+
+		v := binary.BigEndian.Uint16(b[1:3])
+		return uint64(v), 3
+
+	case 0xfe:
+		if len(b) < 5 {
+			return 0, 0
+		}
+
+		v := binary.BigEndian.Uint32(b[1:5])
+		return uint64(v), 5
+
+	case 0xff:
+		if len(b) < 9 {
+			return 0, 0
+		}
+
+		v := binary.BigEndian.Uint64(b[1:9])
+		return v, 9
+	}
+}
+
+// PutUint32 appends a uint32 to b and returns the number of bytes written.
+func PutUint32(b []byte, v uint32) int {
+	return PutUint64(b, uint64(v))
+}
+
+// PutUint32 appends a uint64 to b and returns the number of bytes written.
+func PutUint64(b []byte, v uint64) int {
+	switch {
+	case v <= 0xfc:
+		b[0] = byte(v)
+		return 1
+
+	case v <= 0xffff:
+		b[0] = 0xfd
+		binary.BigEndian.PutUint16(b[1:3], uint16(v))
+		return 3
+
+	case v <= 0xffffffff:
+		b[0] = 0xfe
+		binary.BigEndian.PutUint32(b[1:5], uint32(v))
+		return 5
+
+	default:
+		b[0] = 0xff
+		binary.BigEndian.PutUint64(b[1:9], v)
+		return 9
+	}
+}
---
+++ encoding/compactint/int_test.go
@@ -0,0 +1,67 @@
+package compactint
+
+import (
+	"math"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// Uint32
+
+func TestUint32__should_return_minus_one_on_overflow(t *testing.T) {
+	b := make([]byte, MaxLen)
+	n := PutUint64(b, math.MaxUint64)
+	b = b[:n]
+
+	v, n := Uint32(b)
+	assert.Equal(t, uint32(0), v)
+	assert.Equal(t, -1, n)
+}
+
+// Uint64
+
+func TestUint64__should_read_write_uvarint(t *testing.T) {
+	fn := func(v uint64) {
+		buf := make([]byte, MaxLen)
+		n := PutUint64(buf, v)
+		v1, n1 := Uint64(buf)
+
+		if v != v1 {
+			t.Errorf("Uint64(%d): got %d", v, v1)
+		}
+		if n != n1 {
+			t.Errorf("Uint64(%d): expected n=%d; actual=%d", v, n, n1)
+		}
+	}
+
+	tests := []uint64{
+		0,
+		1,
+		2,
+		10,
+		20,
+		63,
+		64,
+		65,
+		127,
+		128,
+		129,
+		255,
+		256,
+		257,
+		math.MaxUint64 - 1,
+		math.MaxUint64,
+	}
+
+	for _, v := range tests {
+		fn(v)
+	}
+}
+
+func TestUint64__should_return_n_zero_on_small_buffer(t *testing.T) {
+	b := []byte{}
+	v, n := Uint64(b)
+	assert.Equal(t, uint64(0), v)
+	assert.Equal(t, 0, n)
+}
