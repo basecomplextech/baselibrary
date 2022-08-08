85182eda9c9b6d645ec7cc311ef263086fe2d3a7
Ivan Korobkov <ivan.korobkov@gmail.com>
2022-08-08 11:24:17 +0300

    units: Human bytes

---
+++ units/bytes.go
@@ -0,0 +1,48 @@
+package units
+
+import (
+	"fmt"
+	"math"
+)
+
+// Bytes formats a byte count into a human readable string.
+// TODO: Test bytes, including negative bytes.
+type Bytes int64
+
+func (b Bytes) String() string {
+	return ByteString(int64(b))
+}
+
+// ByteString produces a human readable representation of a byte count.
+//
+// ByteString(82854982) -> 79mb
+func ByteString(s int64) string {
+	sizes := []string{"b", "kb", "mb", "gb", "tb", "pb", "eb"}
+	return byteString(s, 1024, sizes)
+}
+
+func byteString(s int64, base float64, sizes []string) string {
+	if s < 10 {
+		return fmt.Sprintf("%db", s)
+	}
+
+	e := math.Floor(logn(float64(s), base))
+	var suffix string
+	if e < float64(len(sizes)) {
+		suffix = sizes[int(e)]
+	} else {
+		suffix = sizes[len(sizes)-1]
+	}
+
+	val := math.Floor(float64(s)/math.Pow(base, e)*10+0.5) / 10
+	f := "%.0f%s"
+	if val < 10 {
+		f = "%.1f%s"
+	}
+
+	return fmt.Sprintf(f, val, suffix)
+}
+
+func logn(n, b float64) float64 {
+	return math.Log(n) / math.Log(b)
+}
