391fe7cc2490a52fada53c830c1c8c6c4989eeda
Ivan Korobkov <ivan.korobkov@gmail.com>
2022-12-31 17:32:24 +0800

    collect/pqueue: Unique priority queue

--- collect/pqueue/queue.go
+++ collect/pqueue/queue.go
@@ -32,7 +32,7 @@ func New[V any, P any](compare compare.Func[P], items ...Item[V, P]) *Queue[V, P
 	return q
 }
 
-// Ordered returns a new priority queue with a priority natural ordering.
+// Ordered returns a new priority queue with the priority natural order.
 func Ordered[V any, P constraints.Ordered](items ...Item[V, P]) *Queue[V, P] {
 	compare := compare.Ordered[P]()
 	return New(compare, items...)
@@ -43,18 +43,17 @@ func (q *Queue[V, P]) Len() int {
 	return q.queue.Len()
 }
 
-// Pop removes and returns the minimum element (according to Less) from the heap.
-// Pop is equivalent to Remove(h, 0).
-func (q *Queue[V, P]) Pop() (value V, ok bool) {
+// Pop removes and returns the minimum element (according to Less) from the queue.
+func (q *Queue[V, P]) Pop() (value V, priority P, ok bool) {
 	if q.Len() == 0 {
-		return value, false
+		return value, priority, false
 	}
 
 	item := heap.Pop(q.queue).(Item[V, P])
-	return item.Value, true
+	return item.Value, item.Priority, true
 }
 
-// Push pushes the element x onto the heap.
+// Push pushes an element onto the queue.
 func (q *Queue[V, P]) Push(value V, priority P) {
 	item := Item[V, P]{
 		Value:    value,
@@ -64,12 +63,41 @@ func (q *Queue[V, P]) Push(value V, priority P) {
 	heap.Push(q.queue, item)
 }
 
-// // Remove removes and returns the element at index i from the heap.
-// func (q *Queue[V, P]) Remove(i int) (value V, ok bool) {
-// 	if i >= q.Len() {
-// 		return value, false
-// 	}
-//
-// 	item := heap.Remove(q.queue, i).(Item[V, P])
-// 	return item.Value, true
-// }
+// internal
+
+var _ heap.Interface = (*heapq[any, any])(nil)
+
+type heapq[V any, P any] struct {
+	compare compare.Func[P]
+	items   []Item[V, P]
+}
+
+// Len is the number of elements in the collection.
+func (q *heapq[V, P]) Len() int {
+	return len(q.items)
+}
+
+// Less reports whether the element with index i must sort before the element with index j.
+func (q *heapq[V, P]) Less(i, j int) bool {
+	a, b := q.items[i], q.items[j]
+	return q.compare(a.Priority, b.Priority) < 0
+}
+
+// Swap swaps the elements with indexes i and j.
+func (q *heapq[V, P]) Swap(i, j int) {
+	q.items[i], q.items[j] = q.items[j], q.items[i]
+}
+
+// Push adds x as element Len()
+func (q *heapq[V, P]) Push(x any) {
+	item := x.(Item[V, P])
+	q.items = append(q.items, item)
+}
+
+// Pop removes and return element Len() - 1.
+func (q *heapq[V, P]) Pop() any {
+	n := len(q.items)
+	x := q.items[n-1]
+	q.items = q.items[:n-1]
+	return x
+}
