e69f982283225c114249b05c4f8788becfd7a7ca
Ivan Korobkov <ivan.korobkov@gmail.com>
2023-01-09 21:17:21 +0800

    terminal: Check env for color support, #13

--- terminal/support.go
+++ terminal/support.go
@@ -1,7 +1,9 @@
 package terminal
 
 import (
+	"fmt"
 	"os"
+	"regexp"
 
 	"github.com/mattn/go-isatty"
 )
@@ -14,7 +16,9 @@ type FileDescriptor interface {
 // CheckColor checks that the file descriptor is a terminal and it supports basic color.
 // If the environment variables force no color, then returns false.
 func CheckColor(fd FileDescriptor) bool {
-	if !isatty.IsTerminal(fd.Fd()) {
+	tty := isatty.IsTerminal(fd.Fd())
+	fmt.Println("TTY", fd.Fd(), tty)
+	if !tty {
 		return false
 	}
 	return SupportsColor()
@@ -22,19 +26,41 @@ func CheckColor(fd FileDescriptor) bool {
 
 // SupportsColor returns true if the environment supports basic color.
 func SupportsColor() bool {
-	// check disabled
-	switch {
-	case hasFlag("no-color"):
-		return false
-	case hasFlag("no-colors"):
-		return false
-	case hasFlag("color=false"):
-		return false
-	case hasFlag("color=never"):
+	if checkColorDisabled() {
 		return false
 	}
+	if checkColorFlags() {
+		return true
+	}
+	return checkColorEnv()
+}
 
-	// check color
+func checkForceColor() bool {
+	s, ok := os.LookupEnv("FORCE_COLOR")
+	switch {
+	case !ok:
+		return false
+	case s == "false":
+		return false
+	}
+	return true
+}
+
+func checkColorDisabled() bool {
+	switch {
+	case hasFlag("no-color"):
+		return true
+	case hasFlag("no-colors"):
+		return true
+	case hasFlag("color=false"):
+		return true
+	case hasFlag("color=never"):
+		return true
+	}
+	return false
+}
+
+func checkColorFlags() bool {
 	switch {
 	case checkForceColor():
 		return true
@@ -58,15 +84,58 @@ func SupportsColor() bool {
 	return false
 }
 
-func checkForceColor() bool {
-	_, ok := os.LookupEnv("FORCE_COLOR")
-	if !ok {
+func checkColorEnv() bool {
+	// dump terminal
+	term := os.Getenv("TERM")
+	if term == "dumb" {
+		return true
+	}
+
+	// ci servers
+	if _, ci := os.LookupEnv("CI"); ci {
+		var names = []string{"TRAVIS", "CIRCLECI", "APPVEYOR", "GITLAB_CI", "GITHUB_ACTIONS",
+			"BUILDKITE", "DRONE"}
+		for _, name := range names {
+			_, ok := os.LookupEnv(name)
+			if ok {
+				return true
+			}
+		}
+
+		if os.Getenv("CI_NAME") == "codeship" {
+			return true
+		}
+
 		return false
 	}
 
-	s := os.Getenv("FORCE_COLOR")
-	if s == "false" {
-		return false
+	// color terminal
+	if _, ok := os.LookupEnv("COLORTERM"); ok {
+		return true
 	}
-	return true
+
+	// terminal program
+	term, ok := os.LookupEnv("TERM_PROGRAM")
+	if ok {
+		switch term {
+		case "iTerm.app":
+			return true
+		case "Apple_Terminal":
+			return true
+		}
+	}
+
+	// term256
+	var term256Regex = regexp.MustCompile("(?i)-256(color)?$")
+	if term256Regex.MatchString(term) {
+		return true
+	}
+
+	// term basic
+	var termBasicRegex = regexp.MustCompile("(?i)^screen|^xterm|^vt100|^vt220|^rxvt|color|ansi|cygwin|linux")
+	if termBasicRegex.MatchString(term) {
+		return true
+	}
+
+	return false
 }
