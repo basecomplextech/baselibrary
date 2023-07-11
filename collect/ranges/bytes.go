0442cf63b554a8bde51a8ec9f79abf53a35bfe86
Ivan Korobkov <ivan.korobkov@gmail.com>
2023-07-11 20:19:17 +0800

    collect/ranges: Rename binary range into bytes range

--- collect/ranges/bytes.go
+++ collect/ranges/bytes.go
@@ -2,11 +2,11 @@ package ranges
 
 import "github.com/complex1tech/baselibrary/compare"
 
-// Binary is a type alias for Range[[]byte].
-type Binary = Range[[]byte]
+// BytesRange is a type alias for Range[[]byte].
+type BytesRange = Range[[]byte]
 
-// ExpandBinary expands a binary range, and returns a new range, skips nil values.
-func ExpandBinary(r Binary, r1 Binary, compare compare.Func[[]byte]) Binary {
+// ExpandBytes expands a bytes range, and returns a new range, skips nil values.
+func ExpandBytes(r BytesRange, r1 BytesRange, compare compare.Func[[]byte]) BytesRange {
 	if r.Start == nil || (r1.Start != nil && compare(r1.Start, r.Start) < 0) {
 		r.Start = r1.Start
 	}
