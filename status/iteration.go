0517c8afe1a342aec42490074beea9b0c9f2aa33
Ivan Korobkov <ivan.korobkov@gmail.com>
2023-11-09 18:01:34 +0800

    status: Endf

--- status/iteration.go
+++ status/iteration.go
@@ -1,6 +1,13 @@
 package status
 
+import "fmt"
+
 var (
 	End  = New(CodeEnd, "")
 	Wait = New(CodeWait, "")
 )
+
+func Endf(format string, a ...any) Status {
+	msg := fmt.Sprintf(format, a...)
+	return Status{Code: CodeEnd, Message: msg}
+}
