536db96a1eeb896b29452a32f8e21c8c2741bb4a
Ivan Korobkov <ivan.korobkov@gmail.com>
2023-12-04 14:01:15 +0800

    async: Atomic bool in flag

--- async/flag.go
+++ async/flag.go
@@ -1,6 +1,9 @@
 package async
 
-import "sync"
+import (
+	"sync"
+	"sync/atomic"
+)
 
 // Flag is a routine-safe boolean flag that can be set, reset, and waited on until set.
 //
@@ -27,7 +30,7 @@ import "sync"
 //	}
 type Flag struct {
 	mu      sync.Mutex
-	set     bool
+	set     atomic.Bool
 	setChan chan struct{} // closed when set
 }
 
@@ -47,10 +50,7 @@ func UnsetFlag() *Flag {
 
 // IsSet returns true if the flag is set.
 func (f *Flag) IsSet() bool {
-	f.mu.Lock()
-	defer f.mu.Unlock()
-
-	return f.set
+	return f.set.Load()
 }
 
 // Set sets the flag, notifies waiters and returns true, or false if already set.
@@ -58,12 +58,12 @@ func (f *Flag) Set() bool {
 	f.mu.Lock()
 	defer f.mu.Unlock()
 
-	if f.set {
+	if f.set.Load() {
 		return false
 	}
 
+	f.set.Store(true)
 	close(f.setChan)
-	f.set = true
 	return true
 }
 
@@ -72,12 +72,12 @@ func (f *Flag) Unset() {
 	f.mu.Lock()
 	defer f.mu.Unlock()
 
-	if !f.set {
+	if !f.set.Load() {
 		return
 	}
 
+	f.set.Store(false)
 	f.setChan = make(chan struct{})
-	f.set = false
 }
 
 // Wait waits for the flag to be set.
