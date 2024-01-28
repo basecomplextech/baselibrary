ef366075f9ab51db0b485b15be99724451544e5c
Ivan Korobkov <ivan.korobkov@gmail.com>
2024-01-28 12:49:53 +0800

    opts: Rename Value into Opt

--- opt/optional.go
+++ opt/optional.go
@@ -1,34 +1,47 @@
 // Package opt provides a generic optional value.
 package opt
 
-// Value is an optional value, an empty value is undefined.
-type Value[T any] struct {
-	Valid bool
+// Opt is an optional value, an empty value is unset.
+type Opt[T any] struct {
+	Set   bool
 	Value T
 }
 
-// New returns a new valid value.
-func New[T any](value T) Value[T] {
-	return Value[T]{
-		Valid: true,
+// New returns a new set value.
+func New[T any](value T) Opt[T] {
+	return Opt[T]{
+		Set:   true,
 		Value: value,
 	}
 }
 
-// None returns a new undefined value.
-func None[T any]() Value[T] {
-	return Value[T]{}
-}
-
-// Unwrap returns the value and true if valid.
-func (v Value[T]) Unwrap() (T, bool) {
-	return v.Value, v.Valid
-}
-
-// MustUnwrap returns the value or panics if not valid.
-func (v Value[T]) MustUnwrap() T {
-	if !v.Valid {
-		panic("undefined value")
+// Maybe returns a new set value if not zero, otherwise an unset value.
+func Maybe[T comparable](value T) Opt[T] {
+	var zero T
+	if value == zero {
+		return Opt[T]{}
 	}
-	return v.Value
+
+	return Opt[T]{
+		Set:   true,
+		Value: value,
+	}
+}
+
+// None returns a new unset value.
+func None[T any]() Opt[T] {
+	return Opt[T]{}
+}
+
+// Unwrap returns the value and true if set.
+func (o Opt[T]) Unwrap() (T, bool) {
+	return o.Value, o.Set
+}
+
+// MustUnwrap returns the value or panics if not set.
+func (o Opt[T]) MustUnwrap() T {
+	if !o.Set {
+		panic("unset value")
+	}
+	return o.Value
 }
