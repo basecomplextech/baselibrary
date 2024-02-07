da684109ba6d76b02f06af2c5bdb1b6379c195b3
Ivan Korobkov <ivan.korobkov@gmail.com>
2024-02-07 12:13:04 +0800

    crypto/blake3: Update comments

--- crypto/blake3/bin128.go
+++ crypto/blake3/bin128.go
@@ -5,17 +5,17 @@ import (
 	"github.com/zeebo/blake3"
 )
 
-// SumBin128 returns a Blake3 hash truncated from 64 bytes to bin128.
+// SumBin128 computes a Blake3 512-bit hash and returns its first 128 bits as a bin128.
 func SumBin128(b []byte) bin.Bin128 {
 	out := blake3.Sum512(b)
 	sum := bin.Bin128{}
-	copy(sum[:], out[:16])
+	copy(sum[:], out[:])
 	return sum
 }
 
 var _ (bin.Hash128) = (*HashBin128)(nil)
 
-// HashBin128 computes a Blake3 hash and truncates it from 64 bytes to bin128.
+// HashBin128 computes a Blake3 512-bit hash and returns its first 128 bits as a bin128.
 type HashBin128 struct {
 	h *blake3.Hasher
 }
--- crypto/blake3/bin256.go
+++ crypto/blake3/bin256.go
@@ -5,15 +5,17 @@ import (
 	"github.com/zeebo/blake3"
 )
 
-// SumBin256 returns a Blake3 hash truncated from 64 bytes to Bin256.
+// SumBin256 computes a Blake3 512-bit hash and returns its first 256 bits as a bin256.
 func SumBin256(b []byte) bin.Bin256 {
-	out := blake3.Sum256(b)
-	return (bin.Bin256)(out)
+	out := blake3.Sum512(b)
+	sum := bin.Bin256{}
+	copy(sum[:], out[:])
+	return sum
 }
 
 var _ (bin.Hash256) = (*HashBin256)(nil)
 
-// HashBin256 computes a Blake3 hash and truncates it from 64 bytes to bin256.
+// HashBin256 computes a Blake3 512-bit hash and returns its first 256 bits as a bin256.
 type HashBin256 struct {
 	h *blake3.Hasher
 }
