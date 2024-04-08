595b21443a4264d438f7f051cfe4ba3b6a3f12b1
Ivan Korobkov <ivan.korobkov@gmail.com>
2024-04-08 18:31:56 +0800

    ref/refqueue: Queue with reference counting

---
+++ ref/refqueue/queue.go
@@ -0,0 +1,105 @@
+package refqueue
+
+import (
+	"sync"
+
+	"github.com/basecomplextech/baselibrary/async"
+	"github.com/basecomplextech/baselibrary/ref"
+)
+
+// Queue is a wrapper around async.Queue with reference counting.
+type Queue[T ref.Ref] interface {
+	async.Queue[T]
+
+	// Free closes and frees the queue.
+	Free()
+}
+
+// New returns a new queue.
+func New[T ref.Ref]() Queue[T] {
+	return newQueue[T]()
+}
+
+// internal
+
+var _ Queue[ref.Ref] = (*queue[ref.Ref])(nil)
+
+type queue[T ref.Ref] struct {
+	mu sync.Mutex
+	q  async.Queue[T]
+}
+
+func newQueue[T ref.Ref]() *queue[T] {
+	return &queue[T]{
+		q: async.NewQueue[T](),
+	}
+}
+
+// Clear clears the queue.
+func (q *queue[T]) Clear() {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+
+	q.clear()
+}
+
+// Close clears and closes the queue.
+func (q *queue[T]) Close() {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+
+	q.clear()
+	q.q.Close()
+}
+
+// Len returns the number of elements in the queue.
+func (q *queue[T]) Len() int {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+
+	return q.q.Len()
+}
+
+// Push adds an element to the queue, panics if the queue is closed.
+func (q *queue[T]) Push(v T) {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+
+	q.q.Push(v)
+	v.Retain()
+}
+
+// Pop removes an element from the queue, returns false if the queue is empty.
+func (q *queue[T]) Pop() (v T, ok bool) {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+
+	return q.q.Pop()
+}
+
+// Wait returns a channel which is notified on new elements.
+func (q *queue[T]) Wait() <-chan struct{} {
+	return q.q.Wait()
+}
+
+// Free closes and frees the queue.
+func (q *queue[T]) Free() {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+
+	q.clear()
+	q.q.Close()
+	q.q = nil
+}
+
+// internal
+
+func (q *queue[T]) clear() {
+	for {
+		v, ok := q.q.Pop()
+		if !ok {
+			break
+		}
+		v.Release()
+	}
+}
---
+++ ref/refqueue/queue_test.go
@@ -0,0 +1,71 @@
+package refqueue
+
+import (
+	"testing"
+
+	"github.com/basecomplextech/baselibrary/ref"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+// Clear
+
+func TestQueue_Clear__should_release_items(t *testing.T) {
+	r := ref.NewNoFreer(123)
+
+	q := newQueue[*ref.R[int]]()
+	q.Push(r)
+	q.Clear()
+
+	assert.Equal(t, int64(1), r.Refcount())
+}
+
+// Close
+
+func TestQueue_Close__should_release_items_close_queue(t *testing.T) {
+	r := ref.NewNoFreer(123)
+
+	q := newQueue[*ref.R[int]]()
+	q.Push(r)
+	q.Close()
+
+	assert.Equal(t, int64(1), r.Refcount())
+}
+
+// Push
+
+func TestQueue_Push__should_retain_item(t *testing.T) {
+	r := ref.NewNoFreer(123)
+
+	q := newQueue[*ref.R[int]]()
+	q.Push(r)
+
+	assert.Equal(t, int64(2), r.Refcount())
+}
+
+// Pop
+
+func TestQueue_Pop__should_pop_item_noop(t *testing.T) {
+	r := ref.NewNoFreer(123)
+
+	q := newQueue[*ref.R[int]]()
+	q.Push(r)
+
+	r1, ok := q.Pop()
+	require.True(t, ok)
+
+	assert.Equal(t, int64(2), r.Refcount())
+	assert.Same(t, r, r1)
+}
+
+// Free
+
+func TestQueue_Free__should_release_items_close_queue(t *testing.T) {
+	r := ref.NewNoFreer(123)
+
+	q := newQueue[*ref.R[int]]()
+	q.Push(r)
+	q.Free()
+
+	assert.Equal(t, int64(1), r.Refcount())
+}
