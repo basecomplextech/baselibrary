8f063f7ef36f2334924f0008861686c0e818a624
Ivan Korobkov <ivan.korobkov@gmail.com>
2024-04-18 10:34:45 +0800

    collect/sets: Diff and intersect

--- collect/sets/set.go
+++ collect/sets/set.go
@@ -1,47 +1,79 @@
 package sets
 
-// Set is a collection of unique values implemented as a map[T]struct{}.
+// Set is a collection of unique items implemented as a map[T]struct{}.
 type Set[T comparable] map[T]struct{}
 
 // New returns a new set.
-func New[T comparable](uu ...T) Set[T] {
-	set := make(Set[T], len(uu))
-	for _, u := range uu {
-		set.Add(u)
+func New[T comparable](items ...T) Set[T] {
+	set := make(Set[T], len(items))
+	for _, item := range items {
+		set.Add(item)
 	}
 	return set
 }
 
-// Add adds values to the set.
-func (s Set[T]) Add(uu ...T) {
-	for _, u := range uu {
-		s[u] = struct{}{}
+// Add adds items to the set.
+func (s Set[T]) Add(item T) {
+	s[item] = struct{}{}
+}
+
+// AddMany adds items to the set.
+func (s Set[T]) AddMany(items ...T) {
+	for _, item := range items {
+		s[item] = struct{}{}
 	}
 }
 
-// Remove removes values from the set.
-func (s Set[T]) Remove(uu ...T) {
-	for _, u := range uu {
-		delete(s, u)
+// Remove removes items from the set.
+func (s Set[T]) Remove(item T) {
+	delete(s, item)
+}
+
+// RemoveMany removes items from the set.
+func (s Set[T]) RemoveMany(items ...T) {
+	for _, item := range items {
+		delete(s, item)
 	}
 }
 
 // Contains returns whether a value is present in the set.
-func (s Set[T]) Contains(u T) bool {
-	_, ok := s[u]
+func (s Set[T]) Contains(item T) bool {
+	_, ok := s[item]
 	return ok
 }
 
 // Clone returns a set clone.
 func (s Set[T]) Clone() Set[T] {
 	s1 := make(Set[T], len(s))
-	for k := range s {
-		s1[k] = struct{}{}
+	for item := range s {
+		s1[item] = struct{}{}
 	}
 	return s1
 }
 
-// ToSlice returns a slice with this set values.
+// Diff returns items that are in this set but not in the other.
+func (s Set[T]) Diff(s1 Set[T]) Set[T] {
+	result := make(Set[T], len(s))
+	for item := range s {
+		if _, ok := s1[item]; !ok {
+			result.Add(item)
+		}
+	}
+	return result
+}
+
+// Intersect returns items that are in both sets.
+func (s Set[T]) Intersect(s1 Set[T]) Set[T] {
+	result := make(Set[T], len(s))
+	for item := range s {
+		if _, ok := s1[item]; ok {
+			result.Add(item)
+		}
+	}
+	return result
+}
+
+// ToSlice returns a slice with this set items.
 func (s Set[T]) ToSlice() []T {
 	vv := make([]T, 0, len(s))
 	for v := range s {
