d8fb4f33a1e220e8317a612a3c15868ac03cc296
Ivan Korobkov <ivan.korobkov@gmail.com>
2024-05-31 16:27:47 +0800

    async: More map benchmarks

--- async/bench_concurrent_map_test.go
+++ async/bench_concurrent_map_test.go
@@ -8,11 +8,11 @@ import (
 
 // Read
 
-func BenchmarkMap_Read(b *testing.B) {
-	m := NewMap[int, int]()
+func BenchmarkConcurrentMap_Read(b *testing.B) {
+	m := NewConcurrentMap[int, int]()
 	items := benchMapItems(1024)
 	for _, item := range items {
-		m.Set(item, item)
+		m.Store(item, item)
 	}
 	b.ResetTimer()
 
@@ -20,9 +20,14 @@ func BenchmarkMap_Read(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		item := items[j]
 
-		_, ok := m.Get(item)
+		_, ok := m.Load(item)
 		if !ok {
 			b.Fatal("item not found")
+		}
+
+		j++
+		if j >= len(items) {
+			j = 0
 		}
 	}
 
@@ -31,11 +36,11 @@ func BenchmarkMap_Read(b *testing.B) {
 	b.ReportMetric(ops/1000_000, "mops")
 }
 
-func BenchmarkMap_Read_Parallel(b *testing.B) {
-	m := NewMap[int, int]()
+func BenchmarkConcurrentMap_Read_Parallel(b *testing.B) {
+	m := NewConcurrentMap[int, int]()
 	items := benchMapItems(1024)
 	for _, item := range items {
-		m.Set(item, item)
+		m.Store(item, item)
 	}
 	b.ResetTimer()
 
@@ -47,7 +52,7 @@ func BenchmarkMap_Read_Parallel(b *testing.B) {
 		for p.Next() {
 			item := items1[j]
 
-			_, ok := m.Get(item)
+			_, ok := m.Load(item)
 			if !ok {
 				b.Fatal("item not found")
 			}
@@ -62,22 +67,22 @@ func BenchmarkMap_Read_Parallel(b *testing.B) {
 	sec := b.Elapsed().Seconds()
 	ops := float64(b.N) / sec
 	b.ReportMetric(ops/1000_000, "mops")
-
 }
 
 // Write
 
-func BenchmarkMap_Write(b *testing.B) {
-	m := NewMap[int, int]()
+func BenchmarkConcurrentMap_Write(b *testing.B) {
+	m := NewConcurrentMap[int, int]()
 	items := benchMapItems(1024)
+
 	b.ResetTimer()
 
 	var j int
 	for i := 0; i < b.N; i++ {
 		item := items[j]
 
-		m.Set(item, item)
-		_, _ = m.Get(item)
+		m.Store(item, item)
+		_, _ = m.Load(item)
 		m.Delete(item)
 
 		j++
@@ -91,21 +96,21 @@ func BenchmarkMap_Write(b *testing.B) {
 	b.ReportMetric(ops/1000_000, "mops")
 }
 
-func BenchmarkMap_Write_Parallel(b *testing.B) {
-	m := NewMap[int, int]()
+func BenchmarkConcurrentMap_Write_Parallel(b *testing.B) {
+	m := NewConcurrentMap[int, int]()
 	items := benchMapItems(1024)
 	b.ResetTimer()
 
 	b.RunParallel(func(p *testing.PB) {
 		items1 := slices.Clone(items)
 		slices.Shuffle(items1)
-
 		var j int
-		for p.Next() {
-			item := items1[j]
 
-			m.Set(item, item)
-			_, _ = m.Get(item)
+		for p.Next() {
+			item := items[j]
+
+			m.Store(item, item)
+			_, _ = m.Load(item)
 			m.Delete(item)
 
 			j++
@@ -118,14 +123,4 @@ func BenchmarkMap_Write_Parallel(b *testing.B) {
 	sec := b.Elapsed().Seconds()
 	ops := float64(b.N) / sec
 	b.ReportMetric(ops/1000_000, "mops")
-}
-
-// private
-
-func benchMapItems(n int) []int {
-	items := make([]int, n)
-	for i := 0; i < n; i++ {
-		items[i] = i
-	}
-	return items
 }
