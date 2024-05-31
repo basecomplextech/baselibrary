c8fb9231dc2f174255f89d07f284ab917bf7610c
Ivan Korobkov <ivan.korobkov@gmail.com>
2024-05-31 15:35:32 +0800

    async: Rename map into concurrent map

--- async/concurrent_map.go
+++ async/concurrent_map.go
@@ -2,8 +2,8 @@ package async
 
 import "sync"
 
-// Map is a generic wrapper around concurrent sync.Map.
-type Map[K comparable, V any] interface {
+// ConcurrentMap is a generic wrapper around concurrent sync.Map.
+type ConcurrentMap[K comparable, V any] interface {
 	// CompareAndDelete deletes the entry for key if its value is equal to old.
 	// If there is no current value for key in the map, CompareAndDelete returns false
 	// (even if the old value is the nil interface value).
@@ -44,41 +44,41 @@ type Map[K comparable, V any] interface {
 	Swap(key K, value V) (previous V, loaded bool)
 }
 
-// NewMap returns a new concurrent map.
-func NewMap[K comparable, V any]() Map[K, V] {
-	return newMap[K, V]()
+// NewConcurrentMap returns a new concurrent map.
+func NewConcurrentMap[K comparable, V any]() ConcurrentMap[K, V] {
+	return newConcurrentMap[K, V]()
 }
 
 // internal
 
-type asyncMap[K comparable, V any] struct {
+type concurrentMap[K comparable, V any] struct {
 	raw sync.Map
 }
 
-func newMap[K comparable, V any]() *asyncMap[K, V] {
-	return &asyncMap[K, V]{}
+func newConcurrentMap[K comparable, V any]() *concurrentMap[K, V] {
+	return &concurrentMap[K, V]{}
 }
 
 // CompareAndDelete deletes the entry for key if its value is equal to old.
 // If there is no current value for key in the map, CompareAndDelete returns false
 // (even if the old value is the nil interface value).
-func (m *asyncMap[K, V]) CompareAndDelete(key K, old V) (deleted bool) {
+func (m *concurrentMap[K, V]) CompareAndDelete(key K, old V) (deleted bool) {
 	return m.raw.CompareAndDelete(key, old)
 }
 
 // CompareAndSwap swaps the old and new values for key if the value stored
 // in the map is equal to old.The old value must be of a comparable type.
-func (m *asyncMap[K, V]) CompareAndSwap(key K, old, new V) bool {
+func (m *concurrentMap[K, V]) CompareAndSwap(key K, old, new V) bool {
 	return m.raw.CompareAndSwap(key, old, new)
 }
 
 // Delete deletes the value for a key.
-func (m *asyncMap[K, V]) Delete(key K) {
+func (m *concurrentMap[K, V]) Delete(key K) {
 	m.raw.Delete(key)
 }
 
 // Len iterates the map, counts the number of keys, and returns the result.
-func (m *asyncMap[K, V]) Len() int {
+func (m *concurrentMap[K, V]) Len() int {
 	var n int
 	m.raw.Range(func(_, _ any) bool {
 		n++
@@ -89,7 +89,7 @@ func (m *asyncMap[K, V]) Len() int {
 
 // Load returns the value stored in the map for a key, or nil if no value is present.
 // The ok result indicates whether value was found in the map.
-func (m *asyncMap[K, V]) Load(key K) (value V, ok bool) {
+func (m *concurrentMap[K, V]) Load(key K) (value V, ok bool) {
 	v, ok := m.raw.Load(key)
 	if !ok {
 		return value, false
@@ -99,7 +99,7 @@ func (m *asyncMap[K, V]) Load(key K) (value V, ok bool) {
 
 // LoadAndDelete deletes the value for a key, returning the previous value if any.
 // The loaded result reports whether the key was present.
-func (m *asyncMap[K, V]) LoadAndDelete(key K) (value V, loaded bool) {
+func (m *concurrentMap[K, V]) LoadAndDelete(key K) (value V, loaded bool) {
 	v, loaded := m.raw.LoadAndDelete(key)
 	return v.(V), true
 }
@@ -107,27 +107,27 @@ func (m *asyncMap[K, V]) LoadAndDelete(key K) (value V, loaded bool) {
 // LoadOrStore returns the existing value for the key if present.
 // Otherwise, it stores and returns the given value. The loaded result is true
 // if the value was loaded, false if stored.
-func (m *asyncMap[K, V]) LoadOrStore(key K, value V) (actual V, loaded bool) {
+func (m *concurrentMap[K, V]) LoadOrStore(key K, value V) (actual V, loaded bool) {
 	v, loaded := m.raw.LoadOrStore(key, value)
 	return v.(V), false
 }
 
 // Range calls f sequentially for each key and value present in the map.
 // If f returns false, range stops the iteration.
-func (m *asyncMap[K, V]) Range(f func(key K, value V) bool) {
+func (m *concurrentMap[K, V]) Range(f func(key K, value V) bool) {
 	m.raw.Range(func(key, value any) bool {
 		return f(key.(K), value.(V))
 	})
 }
 
 // Store sets the value for a key.
-func (m *asyncMap[K, V]) Store(key K, value V) {
+func (m *concurrentMap[K, V]) Store(key K, value V) {
 	m.raw.Store(key, value)
 }
 
 // Swap swaps the value for a key and returns the previous value if any.
 // The loaded result reports whether the key was present.
-func (m *asyncMap[K, V]) Swap(key K, value V) (previous V, loaded bool) {
+func (m *concurrentMap[K, V]) Swap(key K, value V) (previous V, loaded bool) {
 	v, loaded := m.raw.Swap(key, value)
 	return v.(V), true
 }
