d728644e29544bbdf61f95a4efe7729f9453713e
Ivan Korobkov <ivan.korobkov@gmail.com>
2024-08-30 21:08:55 +0800

    crypto/xxh3: Sum32

--- crypto/xxh3/uint64.go
+++ crypto/xxh3/uint64.go
@@ -4,10 +4,17 @@ import (
 	"github.com/zeebo/xxh3"
 )
 
+// Sum32 returns a XXH3 hash high bits as uint32.
+func Sum32(b []byte) uint32 {
+	return uint32(xxh3.Hash(b) >> 32)
+}
+
 // Sum64 returns a XXH3 hash as uint64.
 func Sum64(b []byte) uint64 {
 	return xxh3.Hash(b)
 }
+
+// Hash64
 
 // Hash64 computes an XXH3 hash and returns it as uint64.
 type Hash64 struct {
@@ -19,6 +26,11 @@ type Hash64 struct {
 func (h *Hash64) Write(p []byte) (int, error) {
 	h.h.Write(p)
 	return len(p), nil
+}
+
+// Sum32 returns the current hash high bits as uint32.
+func (h *Hash64) Sum32() uint32 {
+	return uint32(h.h.Sum64() >> 32)
 }
 
 // Sum64 returns the current hash as uint64.
