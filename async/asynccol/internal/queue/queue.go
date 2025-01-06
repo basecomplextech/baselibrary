af476af91ddd91a10360bb31699a15fceee64052
Ivan Korobkov <ivan.korobkov@gmail.com>
2025-01-06 14:42:58 +0800

    async: Update queue

---
+++ async/asynccol/internal/queue/queue.go
@@ -0,0 +1,104 @@
+// Copyright 2023 Ivan Korobkov. All rights reserved.
+// Use of this software is governed by the MIT License
+// that can be found in the LICENSE file.
+
+package queue
+
+import (
+	"sync"
+
+	"github.com/basecomplextech/baselibrary/collect"
+	"github.com/basecomplextech/baselibrary/collect/chans"
+)
+
+// Queue is an unbounded FIFO queue guarded by a mutex.
+type Queue[T any] interface {
+	collect.Queue[T]
+
+	// Wait returns a channel which is notified on new elements.
+	Wait() <-chan struct{}
+}
+
+// New returns an empty queue.
+func New[T any]() Queue[T] {
+	return newQueue[T]()
+}
+
+// internal
+
+var _ Queue[int] = (*queue[int])(nil)
+
+type queue[T any] struct {
+	mu    sync.RWMutex
+	queue collect.Queue[T]
+	wait  chan struct{}
+}
+
+func newQueue[T any]() *queue[T] {
+	return &queue[T]{
+		queue: collect.NewQueue[T](),
+		wait:  make(chan struct{}, 1),
+	}
+}
+
+// Read
+
+// Len returns the number of elements in the queue.
+func (q *queue[T]) Len() int {
+	q.mu.RLock()
+	defer q.mu.RUnlock()
+
+	return q.queue.Len()
+}
+
+// Clear clears the queue.
+func (q *queue[T]) Clear() {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+
+	// Clear queue
+	q.queue.Clear()
+
+	// Notify waiters
+	for len(q.wait) > 0 {
+		select {
+		case <-q.wait:
+		default:
+			break
+		}
+	}
+}
+
+// Push adds an element to the queue.
+func (q *queue[T]) Push(v T) {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+
+	// Push item
+	q.queue.Push(v)
+
+	// Notify waiter
+	select {
+	case q.wait <- struct{}{}:
+	default:
+	}
+}
+
+// Poll removes an element from the queue, returns false if the queue is empty.
+func (q *queue[T]) Poll() (v T, ok bool) {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+
+	return q.queue.Poll()
+}
+
+// Wait returns a channel which is notified on new elements.
+func (q *queue[T]) Wait() <-chan struct{} {
+	q.mu.RLock()
+	defer q.mu.RUnlock()
+
+	if q.queue.Len() > 0 {
+		return chans.Closed()
+	}
+	return q.wait
+}
--- async/asynccol/queue.go
+++ async/asynccol/queue.go
@@ -2,14 +2,14 @@
 // Use of this software is governed by the MIT License
 // that can be found in the LICENSE file.
 
-package async
+package asynccol
 
-import "github.com/basecomplextech/baselibrary/async/internal/queue"
+import "github.com/basecomplextech/baselibrary/async/asynccol/internal/queue"
 
 // Queue is an unbounded FIFO queue.
 type Queue[T any] = queue.Queue[T]
 
 // NewQueue returns an empty queue.
-func NewQueue[T any](items ...T) Queue[T] {
-	return queue.New(items...)
+func NewQueue[T any]() Queue[T] {
+	return queue.New[T]()
 }
