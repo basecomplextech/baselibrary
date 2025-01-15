d16371e381e8db720d663bc2404d00adb51ad4a4
Ivan Korobkov <ivan.korobkov@gmail.com>
2025-01-15 18:17:52 +0800

    retry: Sample10

--- retry/retry_error.go
+++ retry/retry_error.go
@@ -21,3 +21,10 @@ type ErrorFunc func(msg string, err status.Status, attempt int) status.Status
 func (f ErrorFunc) RetryError(msg string, err status.Status, attempt int) status.Status {
 	return f(msg, err, attempt)
 }
+
+// Sample
+
+// Sample10 returns true for every tenth attempt.
+func Sample10(attempt int) bool {
+	return attempt == 0 || attempt%10 == 0
+}
