14dea0a7c7c4b8c62bd8bff36cd8c259f2a3fe1f
Ivan Korobkov <ivan.korobkov@gmail.com>
2025-01-27 14:24:52 +0800

    compare: Clean up types

--- compare/compare.go
+++ compare/compare.go
@@ -8,49 +8,61 @@ import (
 	"github.com/basecomplextech/baselibrary/status"
 )
 
-// Compare compares to values and returns the result.
-// The result should be 0 if a == b, negative if a < b, and positive if a > b.
-type Compare[T any] func(a, b T) int
+type (
+	// Compare compares to values and returns the result.
+	// The result should be 0 if a == b, negative if a < b, and positive if a > b.
+	Compare[T any] func(a, b T) int
 
-// CompareError compares to values and returns the result, or an error.
-// The result should be 0 if a == b, negative if a < b, and positive if a > b.
-type CompareError[T any] func(a, b T) (int, error)
+	// CompareError compares to values and returns the result, or an error.
+	// The result should be 0 if a == b, negative if a < b, and positive if a > b.
+	CompareError[T any] func(a, b T) (int, error)
 
-// CompareStatus compares to values and returns the result and a status.
-// The result should be 0 if a == b, negative if a < b, and positive if a > b.
-type CompareStatus[T any] func(a, b T) (int, status.Status)
+	// CompareStatus compares to values and returns the result and a status.
+	// The result should be 0 if a == b, negative if a < b, and positive if a > b.
+	CompareStatus[T any] func(a, b T) (int, status.Status)
+)
 
 // CompareTo
 
-// CompareTo compares a value to another and returns the result.
-// The result should be 0 if value == another, <0 if value < another, and >0 if value > another.
-//
-// Example:
-//
-//	var another int
-//	func(value int) int {
-//		return value - another
-//	}
-type CompareTo[T any] func(value T) int
+type (
+	// CompareTo compares a value to another and returns the result.
+	// The result should be 0 if value == another, <0 if value < another, and >0 if value > another.
+	//
+	// Example:
+	//
+	//	var another int
+	//	func(value int) int {
+	//		return value - another
+	//	}
+	CompareTo[T any] func(value T) int
 
-// CompareToError compares a value to another and returns the result, or an error.
-// The result should be 0 if value == another, <0 if value < another, and >0 if value > another.
-//
-// Example:
-//
-//	var another int
-//	func(value int) (int, error) {
-//		return value - another, nil
-//	}
-type CompareToError[T any] func(value T) (int, error)
+	// CompareToError compares a value to another and returns the result, or an error.
+	// The result should be 0 if value == another, <0 if value < another, and >0 if value > another.
+	//
+	// Example:
+	//
+	//	var another int
+	//	func(value int) (int, error) {
+	//		return value - another, nil
+	//	}
+	CompareToError[T any] func(value T) (int, error)
 
-// CompareToStatus compares a value to another and returns the result and a status.
-// The result should be 0 if value == another, <0 if value < another, and >0 if value > another.
-//
-// Example:
-//
-//	var another int
-//	func(value int) (int, status.Status) {
-//		return value - another, status.OK
-//	}
-type CompareToStatus[T any] func(value T) (int, status.Status)
+	// CompareToStatus compares a value to another and returns the result and a status.
+	// The result should be 0 if value == another, <0 if value < another, and >0 if value > another.
+	//
+	// Example:
+	//
+	//	var another int
+	//	func(value int) (int, status.Status) {
+	//		return value - another, status.OK
+	//	}
+	CompareToStatus[T any] func(value T) (int, status.Status)
+)
+
+// Aliases
+
+type (
+	CompareBytes       = Compare[[]byte]
+	CompareBytesError  = CompareError[[]byte]
+	CompareBytesStatus = CompareStatus[[]byte]
+)
