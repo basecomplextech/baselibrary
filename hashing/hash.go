8d142131d66b5e17ad6ef9c02f5c52cad4992961
Ivan Korobkov <ivan.korobkov@gmail.com>
2026-03-27 14:13:15 +0300

    hashing: Hasher interface

--- hashing/hash.go
+++ hashing/hash.go
@@ -12,6 +12,64 @@ import (
 	"github.com/basecomplextech/baselibrary/bin"
 )
 
+// Hash returns a hash of a key, panics if the key type is not supported.
+// Add more types as needed.
+func Hash[K any](key K) uint32 {
+	switch v := any(key).(type) {
+	case bool:
+		if v {
+			return 1
+		}
+		return 0
+
+	case int:
+		return uint32(v ^ (v >> 32)) // xor of two halves
+	case int8:
+		return uint32(v)
+	case int16:
+		return uint32(v)
+	case int32:
+		return uint32(v)
+	case int64:
+		return uint32(v ^ (v >> 32)) // xor of two halves
+
+	case uint:
+		return uint32(v ^ (v >> 32)) // xor of two halves
+	case uint8:
+		return uint32(v)
+	case uint16:
+		return uint32(v)
+	case uint32:
+		return uint32(v)
+	case uint64:
+		return uint32(v ^ (v >> 32)) // xor of two halves
+
+	case float32:
+		return math.Float32bits(v)
+	case float64:
+		v1 := math.Float64bits(v)
+		return uint32(v1 ^ (v1 >> 32)) // xor of two halves
+
+	case string:
+		return HashString(v)
+	case []byte:
+		return HashBytes(v)
+
+	case bin.Bin64:
+		return v.Hash32()
+	case bin.Bin128:
+		return v.Hash32()
+	case bin.Bin256:
+		return v.Hash32()
+	}
+
+	if h, ok := ((any)(key)).(Hashable); ok {
+		return h.Hash32()
+	}
+
+	panic(fmt.Sprintf("unsupported type %T", key))
+}
+
 // HashBool returns a hash of a bool value.
 func HashBool(b bool) uint32 {
 	if b {
@@ -19,6 +77,32 @@ func HashBool(b bool) uint32 {
 	}
 	return 0
 }
+
+// HashInt returns a hash of an int value.
+func HashInt(i int) uint32     { return uint32(i ^ (i >> 32)) } // xor of two halves
+func HashInt8(i int8) uint32   { return uint32(i) }
+func HashInt16(i int16) uint32 { return uint32(i) }
+func HashInt32(i int32) uint32 { return uint32(i) }
+func HashInt64(i int64) uint32 { return uint32(i ^ (i >> 32)) } // xor of two halves
+
+// HashUint returns a hash of a uint value.
+func HashUint(u uint) uint32     { return uint32(u ^ (u >> 32)) } // xor of two halves
+func HashUint8(u uint8) uint32   { return uint32(u) }
+func HashUint16(u uint16) uint32 { return uint32(u) }
+func HashUint32(u uint32) uint32 { return u }
+func HashUint64(u uint64) uint32 { return uint32(u ^ (u >> 32)) } // xor of two halves
+
+// HashFloat returns a hash of a float value.
+func HashFloat32(f float32) uint32 { return math.Float32bits(f) }
+func HashFloat64(f float64) uint32 {
+	v := math.Float64bits(f)
+	return uint32(v ^ (v >> 32)) // xor of two halves
+}
+
+// HashBin returns a hash of a bin value.
+func HashBin64(b bin.Bin64) uint32   { return b.Hash32() }
+func HashBin128(b bin.Bin128) uint32 { return b.Hash32() }
+func HashBin256(b bin.Bin256) uint32 { return b.Hash32() }
 
 // HashBytes returns a hash of a byte slice value.
 func HashBytes(b []byte) uint32 {
@@ -57,61 +141,4 @@ func HashPointer(v any) uint32 {
 
 	h := uintptr(e.ptr)
 	return uint32(h ^ (h >> 32)) // xor of two halves
-}
-
-// Hash returns a hash of a key, panics if the key type is not supported.
-// Add more types as needed.
-func Hash[K any](key K) uint32 {
-	switch v := any(key).(type) {
-	case bool:
-		if v {
-			return 1
-		}
-		return 0
-
-	case int:
-		return uint32(v)
-
-	case int8:
-		return uint32(v)
-	case int16:
-		return uint32(v)
-	case int32:
-		return uint32(v)
-	case int64:
-		return uint32(v ^ (v >> 32)) // xor of two halves
-
-	case uint8:
-		return uint32(v)
-	case uint16:
-		return uint32(v)
-	case uint32:
-		return uint32(v)
-	case uint64:
-		return uint32(v ^ (v >> 32)) // xor of two halves
-
-	case float32:
-		return math.Float32bits(v)
-	case float64:
-		v1 := math.Float64bits(v)
-		return uint32(v1 ^ (v1 >> 32)) // xor of two halves
-
-	case string:
-		return HashString(v)
-	case []byte:
-		return HashBytes(v)
-
-	case bin.Bin64:
-		return v.Hash32()
-	case bin.Bin128:
-		return v.Hash32()
-	case bin.Bin256:
-		return v.Hash32()
-	}
-
-	if h, ok := ((any)(key)).(Hasher); ok {
-		return h.Hash32()
-	}
-
-	panic(fmt.Sprintf("unsupported type %T", key))
 }
--- hashing/hasher.go
+++ hashing/hasher.go
@@ -4,7 +4,124 @@
 
 package hashing
 
-type Hasher interface {
-	// Hash32 returns a 32-bit hash of the object.
-	Hash32() uint32
+import (
+	"fmt"
+
+	"github.com/basecomplextech/baselibrary/bin"
+)
+
+// Hasher computes a 32-bit hash of a value.
+type Hasher[T any] interface {
+	// Hash32 returns a 32-bit hash of the value.
+	Hash32(v T) uint32
 }
+
+// NewHasher returns a hasher for the given type, panics if the type is not supported.
+func NewHasher[T any]() Hasher[T] {
+	var k T
+
+	switch any(k).(type) {
+	case bool:
+		return (any)(boolHasher{}).(Hasher[T])
+
+	case int:
+		return (any)(intHasher{}).(Hasher[T])
+	case int8:
+		return (any)(int8Hasher{}).(Hasher[T])
+	case int16:
+		return (any)(int16Hasher{}).(Hasher[T])
+	case int32:
+		return (any)(int32Hasher{}).(Hasher[T])
+	case int64:
+		return (any)(int64Hasher{}).(Hasher[T])
+
+	case uint:
+		return (any)(uintHasher{}).(Hasher[T])
+	case uint8:
+		return (any)(uint8Hasher{}).(Hasher[T])
+	case uint16:
+		return (any)(uint16Hasher{}).(Hasher[T])
+	case uint32:
+		return (any)(uint32Hasher{}).(Hasher[T])
+	case uint64:
+		return (any)(uint64Hasher{}).(Hasher[T])
+
+	case float32:
+		return (any)(float32Hasher{}).(Hasher[T])
+	case float64:
+		return (any)(float64Hasher{}).(Hasher[T])
+
+	case bin.Bin64:
+		return (any)(bin64Hasher{}).(Hasher[T])
+	case bin.Bin128:
+		return (any)(bin128Hasher{}).(Hasher[T])
+	case bin.Bin256:
+		return (any)(bin256Hasher{}).(Hasher[T])
+
+	case []byte:
+		return (any)(bytesHasher{}).(Hasher[T])
+	case string:
+		return (any)(stringHasher{}).(Hasher[T])
+
+	case Hashable:
+		return (any)(hashableHasher[T]{}).(Hasher[T])
+	}
+
+	panic(fmt.Sprintf("unsupported hasher type: %T", k))
+}
+
+// internal
+
+type (
+	boolHasher struct{}
+
+	intHasher   struct{}
+	int8Hasher  struct{}
+	int16Hasher struct{}
+	int32Hasher struct{}
+	int64Hasher struct{}
+
+	uintHasher   struct{}
+	uint8Hasher  struct{}
+	uint16Hasher struct{}
+	uint32Hasher struct{}
+	uint64Hasher struct{}
+
+	float32Hasher struct{}
+	float64Hasher struct{}
+
+	bin64Hasher  struct{}
+	bin128Hasher struct{}
+	bin256Hasher struct{}
+
+	bytesHasher  struct{}
+	stringHasher struct{}
+
+	hashableHasher[T any] struct{}
+)
+
+func (boolHasher) Hash32(v bool) uint32 { return HashBool(v) }
+
+func (intHasher) Hash32(v int) uint32     { return HashInt(v) }
+func (int8Hasher) Hash32(v int8) uint32   { return HashInt8(v) }
+func (int16Hasher) Hash32(v int16) uint32 { return HashInt16(v) }
+func (int32Hasher) Hash32(v int32) uint32 { return HashInt32(v) }
+func (int64Hasher) Hash32(v int64) uint32 { return HashInt64(v) }
+
+func (uintHasher) Hash32(v uint) uint32     { return HashUint(v) }
+func (uint8Hasher) Hash32(v uint8) uint32   { return HashUint8(v) }
+func (uint16Hasher) Hash32(v uint16) uint32 { return HashUint16(v) }
+func (uint32Hasher) Hash32(v uint32) uint32 { return HashUint32(v) }
+func (uint64Hasher) Hash32(v uint64) uint32 { return HashUint64(v) }
+
+func (float32Hasher) Hash32(v float32) uint32 { return HashFloat32(v) }
+func (float64Hasher) Hash32(v float64) uint32 { return HashFloat64(v) }
+
+func (bin64Hasher) Hash32(v bin.Bin64) uint32   { return HashBin64(v) }
+func (bin128Hasher) Hash32(v bin.Bin128) uint32 { return HashBin128(v) }
+func (bin256Hasher) Hash32(v bin.Bin256) uint32 { return HashBin256(v) }
+
+func (bytesHasher) Hash32(v []byte) uint32  { return HashBytes(v) }
+func (stringHasher) Hash32(v string) uint32 { return HashString(v) }
+
+func (hashableHasher[T]) Hash32(v T) uint32 { return (any)(v).(Hashable).Hash32() }
